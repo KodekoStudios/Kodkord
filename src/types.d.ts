659e8a72bd593b0d0c1d8d38b028e0271caa82ae
Pavez7274 <tuviejale@gmail.com>
2024-06-27 00:12:06 -0300

    refactor(types): improve MakeRequired and Nullable type definitions
    - Updated the MakeRequired type definition to include JSDoc comments for better documentation.
    - Added a new type definition, Nullable, to represent a type that can be nullable or undefined.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,17 @@
-export type MakeRequired<T, K extends keyof T> = T & {
-	[P in K]-?: T[P];
+/**
+ * Makes specified properties of a type required.
+ *
+ * @template Type The original type.
+ * @template Keys The keys of the properties to make required.
+ */
+export type MakeRequired<Type, Keys extends keyof Type> = Type & {
+	// For each key in the original type, if the key is in the specified keys, make it required.
+	[Key in Keys]-?: Type[Key];
 };
 
-export type Nullable<T> = T | null | undefined;
+/**
+ * Represents a type that can be nullable or undefined.
+ *
+ * @template Type The underlying type.
+ */
+export type Nullable<Type> = Type | null | undefined;
