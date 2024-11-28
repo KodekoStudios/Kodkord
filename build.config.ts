6b79e5e384211d3fc7cb43f295689c923c298bd1
Pavez7274 <tuviejale@gmail.com>
2024-11-28 02:40:48 -0300

    minor but major fixes

--- build.config.ts
+++ build.config.ts
@@ -1,28 +1,3 @@
-// import { defineBuildConfig } from "unbuild";
-
-// export default defineBuildConfig({
-// 	entries: [
-// 		"src/",
-// 		{
-// 			input: "src/",
-// 			outDir: "lib/",
-// 			format: "esm",
-// 		},
-// 		{
-// 			input: "src/",
-// 			outDir: "lib/",
-// 			format: "cjs",
-// 			ext: "cjs",
-// 			declaration: false,
-// 		},
-// 	],
-// 	outDir: "lib/",
-// 	declaration: true,
-// 	rollup: {
-// 		emitCJS: true,
-// 	},
-// });
-
 import { defineBuildConfig } from "unbuild";
 import { readdirSync, statSync } from "node:fs";
 import { join, resolve } from "node:path";
