fb250451c1c045d759469be9dfd7f3a96d49fb0c
Pavez7274 <tuviejale@gmail.com>
2025-01-11 03:01:50 -0300

    minor fixes

--- src/structures/channel/group.dm.channel.ts
+++ src/structures/channel/group.dm.channel.ts
@@ -3,12 +3,11 @@ import type { ChannelType } from "discord-api-types/v10";
 import { Channel } from "./channel";
 
 export class GroupDMChannel extends Channel<ChannelType.GroupDM> {
+	public override get name(): Nullable<string> {
+		return super.name;
+	}
 
-    public override get name(): Nullable<string> {
-        return super.name;
-    }
-
-    public get flags(): Nullable<number> {
-        return this.data.flags;
-    }
+	public get flags(): Nullable<number> {
+		return this.data.flags;
+	}
 }
