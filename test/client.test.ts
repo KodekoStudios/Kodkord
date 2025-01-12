46c061840321f70a579e3a8e7ce6f5f53f71f1a6
Aarón Rafael <69669283+Chewawi@users.noreply.github.com>
2025-01-12 01:37:15 -0300

    Revert "updated tests"
    This reverts commit 637de711b75262bf0a79bc57ea348331063980af.

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,13 +1,10 @@
 import { describe, expect, it } from "bun:test";
-import { Client, GatewayIntentBits, GuildTextChannel, User } from "../src/index";
+import { Client, GuildTextChannel, type Message, User } from "../src/index";
 
 describe("Client", () => {
 	const CLIENT = new Client({
 		token: process.env.test_token as string,
 		type: "Bot",
-		defaultPrefix: ["k!"],
-		intents:
-			GatewayIntentBits.Guilds | GatewayIntentBits.GuildMessages | GatewayIntentBits.MessageContent,
 	});
 
 	it("should fetch current user", async () => {
@@ -24,29 +21,31 @@ describe("Client", () => {
 	});
 
 	it("should fetch a channel by Id", async () => {
-		const ID = "913243741710594048";
+		const ID = "1212603668466241576";
 		const CHANNEL = await CLIENT.channels.fetch(ID);
-		console.log("Fetched Channel:", CHANNEL.data);
+		console.log("fetchedChannel:", CHANNEL.data);
 		expect(CHANNEL).toBeInstanceOf(GuildTextChannel);
 		expect(CHANNEL.id).toBe(ID);
 	});
 
 	it("should set a command", () => {
 		CLIENT.commands.set({
-			name: "reply",
-			run: async (message): Promise<void> => {
-				const RES = await message.reply({
-					content: "Testing!",
-				});
-				console.log(RES.data);
+			name: "ping",
+			run: (message): void => {
+				// @ts-expect-error
+				message.reply("Pong!");
 			},
 		});
 
 		expect(CLIENT.commands.get("ping")).toBeDefined();
 	});
 
-	it("should run a command", async () => {
-		const MESSAGE = await CLIENT.messages.fetch("913243741710594048", "1327754552757452910");
-		CLIENT.commands.get("reply")?.run(MESSAGE);
+	it("should run a command", () => {
+		CLIENT.commands.get("ping")?.run({
+			content: "!ping",
+			reply: (content: string): void => {
+				expect(content).toBe("Pong!");
+			},
+		} as unknown as Message);
 	});
 });
