cf93b050c2ba7e06936145ec5343dfe6df205c4b
Aarón Rafael <69669283+Chewawi@users.noreply.github.com>
2025-01-17 23:30:25 -0300

    lint: no null assertion

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -7,6 +7,7 @@ const rules = {
         "perfectionist/sort-classes": "off",
         "perfectionist/sort-switch-case": "off",
         "perfectionist/sort-exports": "off",
+        "@typescript-eslint/non-nullable-type-assertion-style": "off",
         "@stylistic/quotes": "off",
         "marcrock/use-filenaming-convention": ["error", {
             match: /^[a-z0-9]+(?:\.[a-z0-9]+)*\.ts$/
--- packages/kodkord/src/core/sharder.ts
+++ packages/kodkord/src/core/sharder.ts
@@ -39,7 +39,7 @@ export class Sharder extends Dictionary<number, Shard> {
 	public create(id: number, settings?: ShardSettings): Shard {
 		if (this.has(id)) {
 			new Panic("Sharder", `Shard #${id} already exists.`).panic();
-			return this.get(id)!;
+			return this.get(id) as Shard;
 		}
 
 		const SHARD = new Shard(this.client, id, settings);
@@ -80,7 +80,7 @@ export class Sharder extends Dictionary<number, Shard> {
 	 */
 	public forceIdentify(id: number): void {
 		const SHARD = this.has(id)
-			? (this.get(id)!)
+			? (this.get(id) as Shard)
 			: this.create(id);
 
 		if (SHARD.websocket.disconnected()) {
