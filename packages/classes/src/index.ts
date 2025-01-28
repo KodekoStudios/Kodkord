d031ae7bf797568e37bc13f4328563a7c0ca87aa
KingsBeCattz <jdanielxd.3@gmail.com>
2025-01-28 13:28:19 -0800

    The corresponding exports have been added

--- packages/classes/src/index.ts
+++ packages/classes/src/index.ts
@@ -5,3 +5,8 @@ export * from "./entities/channel";
 export * from "./entities/message";
 export * from "./entities/image";
 export * from "./entities/user";
+export * from './entities/emoji';
+export * from './entities/guild';
+export * from './entities/member';
+export * from './entities/role';
+export * from './entities/sticker'
